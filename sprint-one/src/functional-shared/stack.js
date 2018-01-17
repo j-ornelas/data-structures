72a070962ce3503de24bf46364e7e1ae83d4e6f0
j-ornelas <johnnyoh1988@gmail.com>
2018-01-17 15:22:04 -0800

    Solves all except for the last condition

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,23 +1,38 @@
 var Stack = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
   var count = 0;
+  var storage = {}
   var someInstance = {
     size: function() {
       return count < 0 ? 0 : count;
     },
-    push: function() {
-      count++;
+    push: function(value) {
+      storage[count] = value
+      count += 1;
+      return storage[count]
     },
     pop: function () {
+      delete storage[count]
       count -= 1;
+      return storage[count]
     }
-
   }
   return someInstance
-
-  // count = 0;
-
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {someInstance: {size: 0}};
+var stackMethods = {
+  size: function() {
+    return count < 0 ? 0 : count;
+  },
+  push: function(value) {
+    storage[count] = value
+    count += 1;
+    return storage[count]
+  },
+  pop: function () {
+    delete storage[count]
+    count -= 1;
+    return storage[count]
+  }
+};
