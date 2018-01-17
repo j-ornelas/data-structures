2ef04a91c5b48d94544a70274b6fd2cade2cad7f
j-ornelas <johnnyoh1988@gmail.com>
2018-01-17 13:12:46 -0800

    Solves all tests except last for function/queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,24 +1,35 @@
 var Queue = function() {
   var someInstance = {};
   var count = 0;
-
+  var lowestCount = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    
-    count ++;
+    storage[count] = value
+    count += 1;
+    return storage[count]
   };
 
   someInstance.dequeue = function() {
+    // if (count - lowestCount === 0){
+    //   return undefined;
+    // }
+    var toDelete = storage[0]
+    console.log(storage)
+    delete storage[0]
+    lowestCount += 1;
     count -= 1;
+    debugger;
+    console.log(storage)
+    return toDelete;
   };
 
   someInstance.size = function() {
     return count < 0 ? 0 : count;
   };
-
+  console.log(storage)
   return someInstance;
 };
