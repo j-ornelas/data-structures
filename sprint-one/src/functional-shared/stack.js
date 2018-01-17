8f6dd4aabb98d57ab4aa83c1fe2a0242a4cc45ab
j-ornelas <johnnyoh1988@gmail.com>
2018-01-17 15:07:40 -0800

    Gives the function a successful 'count' variable

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
+  var count = 0;
+  var someInstance = {
+    size: function() {
+      return count < 0 ? 0 : count;
+    },
+    push: function() {
+      count++;
+    },
+    pop: function () {
+      count -= 1;
+    }
+
+  }
+  return someInstance
+
+  // count = 0;
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {someInstance: {size: 0}};
