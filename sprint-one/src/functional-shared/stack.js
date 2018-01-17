0e32a9c11fd77164e4637334c59f751670ba5b0f
j-ornelas <johnnyoh1988@gmail.com>
2018-01-17 15:54:47 -0800

    breaks everything, but goes in the right direction

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,38 +1,43 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var count = 0;
-  var storage = {}
-  var someInstance = {
-    size: function() {
-      return count < 0 ? 0 : count;
-    },
-    push: function(value) {
-      storage[count] = value
-      count += 1;
-      return storage[count]
-    },
-    pop: function () {
-      delete storage[count]
-      count -= 1;
-      return storage[count]
-    }
-  }
+  var someInstance = {}
+  someInstance[count] = 0;
+  someInstance[storage] = {};
+  someInstance[size] = stackMethods[size];
+  someInstance[push] = stackMethods[push];
+  someInstance[pop] = stackMethods[pop];
+
+  // var someInstance = {
+  //   size: function() {
+  //     return count < 0 ? 0 : count;
+  //   },
+  //   push: function(value) {
+  //     storage[count] = value
+  //     count += 1;
+  //     return storage[count]
+  //   },
+  //   pop: function () {
+  //     delete storage[count]
+  //     count -= 1;
+  //     return storage[count]
+  //   }
+  // }
   return someInstance
 };
 
 var stackMethods = {
   size: function() {
-    return count < 0 ? 0 : count;
+    return this[count] < 0 ? 0 : count;
   },
   push: function(value) {
     storage[count] = value
-    count += 1;
+    this[count] += 1;
     return storage[count]
   },
   pop: function () {
     delete storage[count]
-    count -= 1;
+    this[count] -= 1;
     return storage[count]
   }
 };
