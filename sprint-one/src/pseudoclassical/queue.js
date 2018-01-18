e1263c4e6dc282058d24a8e9d7aad25c01666fbb
j-ornelas <johnnyoh1988@gmail.com>
2018-01-17 21:57:16 -0800

    Solves all conditions for Prototye / Queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value
+  this.count += 1;
+  return this.storage[this.count]
+}
 
+Queue.prototype.dequeue = function() {
+  var toDelete = this.storage[0];
+    for (key in this.storage){
+      if (this.storage[key] === undefined){
+        break;
+      }
+      this.storage[key - 1] = this.storage[key];
+    }
+  this.count -= 1;
+  return toDelete;
+}
+
+Queue.prototype.size = function() {
+  return this.count < 0 ? 0 : this.count;
+}
