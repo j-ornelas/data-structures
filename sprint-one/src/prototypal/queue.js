08243b304cc2af88134fa68d63a3bbb151bd2e90
j-ornelas <johnnyoh1988@gmail.com>
2018-01-17 21:31:31 -0800

    Solves all conditions for prototype / queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var someInstance = Object.create(queueMethods);
+someInstance.count = 0;
+someInstance.storage = {};
+return someInstance;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.count] = value
+    this.count += 1;
+    return this.storage[this.count]
+  },
+  dequeue: function() {
+    var toDelete = this.storage[0];
+      for (key in this.storage){
+        if (this.storage[key] === undefined){
+          break;
+        }
+        this.storage[key - 1] = this.storage[key];
+      }
+    this.count -= 1;
+    return toDelete;
+  }, size: function() {
+    return this.count < 0 ? 0 : this.count;
+  }
+};
