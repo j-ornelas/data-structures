c50205188f81b36cb83cdb43489823808c2189a6
j-ornelas <johnnyoh1988@gmail.com>
2018-01-19 20:06:58 -0800

    Makes BST add new nodes properly

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,22 +1,43 @@
+//JO REPO
 var BinarySearchTree = function(value) {
-  this.mother = null;
-  console.log(this)
-  
+  // this.mother = {};
+  this.value = value;
+  this.left = null;
+  this.right = null;
+
+
 };
 
 BinarySearchTree.prototype.insert = function(value) {
-  var mother = this.mother;
-  if (mother === null) {
-    this.mother = new Node(value);
-    return;
-  }
+  // var isEmpty = function isEmpty(obj) {
+  //   for (var key in obj) {
+  //     if (obj.hasOwnProperty(key)) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // };
 
-  if (value > mother.value) {
-    mother.right = new Node(value);
-  }
+  // if (isEmpty(this.mother)) {
+  //   this.mother = new BinarySearchTree(value);
+  //   return;
+  // }
 
+  var traverse = function(node) {
+    if (value > node.value && node.right === null) {
+      node.right = new BinarySearchTree(value);
+    } else if (value < node.value && node.left === null) {
+      node.left = new BinarySearchTree(value);
+    } else if (value > node.value && typeof node.right === 'object') {
+      traverse(node.right);
+    } else if (value < node.value && typeof node.left === 'object') {
+      traverse(node.left);
+    }
+
+  };
+  traverse(this);
   
-
+  
 };
 
 BinarySearchTree.prototype.contains = function() {
@@ -29,9 +50,11 @@ BinarySearchTree.prototype.depthFirstLog = function() {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-var Node = function(value) {
+ 
+// var Node = function(value, leftChild = null, rightChild = null, parent = null) {
 
-  this.left = null;
-  this.right = {};
-  this.value = value;
-};
+//   this.left = leftChild;
+//   this.right = rightChild;
+//   this.parent = parent;
+//   this.value = value;
+// };
