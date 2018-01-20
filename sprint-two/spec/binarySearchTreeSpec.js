03da9980a255bbcddf017f34f4da2fa5630d7109
j-ornelas <johnnyoh1988@gmail.com>
2018-01-19 17:48:47 -0800

    binarySearchTree now finds the correct functions

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -12,6 +12,7 @@ describe('binarySearchTree', function() {
   });
 
   it('should insert values at the correct location in the tree', function() {
+
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,37 @@
 var BinarySearchTree = function(value) {
+  this.mother = null;
+  console.log(this)
+  
 };
 
+BinarySearchTree.prototype.insert = function(value) {
+  var mother = this.mother;
+  if (mother === null) {
+    this.mother = new Node(value);
+    return;
+  }
 
+  if (value > mother.value) {
+    mother.right = new Node(value);
+  }
+
+  
+
+};
+
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var Node = function(value) {
+
+  this.left = null;
+  this.right = {};
+  this.value = value;
+};
